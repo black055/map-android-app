87151782a381bebbfd78b8ec1d456fee2b396efd
123prob7 <truongnguyenvan9a9@gmail.com>
2020-11-04 15:36:28 +0700

    Find path between 2 location

--- app/src/main/java/com/example/map_application/MapsActivity.java
+++ app/src/main/java/com/example/map_application/MapsActivity.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.app.ActionBar;
+import android.app.ProgressDialog;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -24,7 +25,9 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -34,8 +37,12 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
@@ -48,23 +55,35 @@ import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import modules.DirectionFinder;
+import modules.DirectionFinderListener;
+import modules.Route;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, DirectionFinderListener {
 
     private final int DEFAULT_MAP_HEIGHT = 17;
     private boolean locationPermissionGranted;
     private GoogleMap mMap;
     private Geocoder geocoder;
-    private Button btnZoomIn, btnZoomOut;
+    private Button btnZoomIn, btnZoomOut, btnFindPathBack, btnFindPath;
     private ImageView btnCurPosition;
     private Location lastLocation;
     private LatLng defaultLocation;
     private SearchView searchLocation;
     private ActionBar toolBar;
+    private EditText edtOrigin, edtDestination;
+    private LinearLayout llFindPath;
+    private List<Marker> originMarkers = new ArrayList<>();
+    private List<Marker> destinationMarkers = new ArrayList<>();
+    private List<Polyline> polylinePaths = new ArrayList<>();
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,8 +113,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Thêm navigation bar
         toolBar = getActionBar();
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.buttom_navigation);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Thêm find path
+        llFindPath = findViewById(R.id.llFindPath);
+        edtOrigin = findViewById(R.id.edtOrigin);
+        edtDestination = findViewById(R.id.edtDestination);
+        btnFindPathBack = findViewById(R.id.btnFindPathBack);
+        btnFindPath = findViewById(R.id.btnFindPath);
+
+        //send request
+        btnFindPath.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequest();
+            }
+        });
     }
 
     // TODO: Sử lý các các tính năng tại đây
@@ -106,6 +140,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             switch (item.getItemId())
             {
                 case R.id.find:
+                    //Ẩn thanh search location
+                    searchLocation.setVisibility(View.GONE);
+                    //Hiện thanh tìm kiếm 2 địa điểm
+                    llFindPath.setVisibility(View.VISIBLE);
+                    //Xử lí nút back quay về giao diện chính
+                    btnFindPathBack.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            llFindPath.setVisibility(View.GONE);
+                            searchLocation.setVisibility(View.VISIBLE);
+                        }
+                    });
                     return true;
                 case R.id.place:
                     return true;
@@ -235,4 +281,83 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
     }
+
+    //Find path-----------
+    @Override
+    public void onDirectionFinderStart() {
+        /*progressDialog = ProgressDialog.show(this, "Please wait.",
+                "Finding direction..!", true);*/
+
+        if (originMarkers != null) {
+            for (Marker marker : originMarkers) {
+                marker.remove();
+            }
+        }
+
+        if (destinationMarkers != null) {
+            for (Marker marker : destinationMarkers) {
+                marker.remove();
+            }
+        }
+
+        if (polylinePaths != null) {
+            for (Polyline polyline:polylinePaths ) {
+                polyline.remove();
+            }
+        }
+    }
+
+    @Override
+    public void onDirectionFinderSuccess(List<Route> routes) {
+        polylinePaths = new ArrayList<>();
+        originMarkers = new ArrayList<>();
+        destinationMarkers = new ArrayList<>();
+
+        if (routes.size() == 0) return;
+
+        for (Route route : routes) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(route.startLocation, 16));
+            ((TextView) findViewById(R.id.tvDuration)).setText(route.duration.text);
+            ((TextView) findViewById(R.id.tvDistance)).setText(route.distance.text);
+
+            originMarkers.add(mMap.addMarker(new MarkerOptions()
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.blue_start))
+                    .title(route.startAddress)
+                    .position(route.startLocation)));
+            destinationMarkers.add(mMap.addMarker(new MarkerOptions()
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.red_end))
+                    .title(route.endAddress)
+                    .position(route.endLocation)));
+
+            PolylineOptions polylineOptions = new PolylineOptions().
+                    geodesic(true).
+                    color(R.color.quantum_lightblue).
+                    width(10);
+
+            for (int i = 0; i < route.points.size(); i++)
+                polylineOptions.add(route.points.get(i));
+
+            polylinePaths.add(mMap.addPolyline(polylineOptions));
+        }
+    }
+
+    private void sendRequest() {
+        String origin = edtOrigin.getText().toString();
+        String destination = edtDestination.getText().toString();
+        if (origin.isEmpty()) {
+            Toast.makeText(this, "Please enter origin address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (destination.isEmpty()) {
+            Toast.makeText(this, "Please enter destination address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        try {
+            new DirectionFinder(this, origin, destination).execute();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+    //-----------------------------------------------------------
 }
